7ea1745aeaad7cad772e635734fdb3f6c76d46c6
VneTraffiqua <safe72again@gmail.com>
2023-08-31 07:58:35 -0700

    Added button

--- tg_bot.py
+++ tg_bot.py
@@ -2,6 +2,7 @@ import os
 import logging
 import redis
 from environs import Env
+from telegram import InlineKeyboardMarkup, InlineKeyboardButton
 from telegram.ext import Filters, Updater
 from telegram.ext import CallbackQueryHandler, CommandHandler, MessageHandler
 
@@ -15,7 +16,16 @@ def start(update, context):
     Бот отвечает пользователю фразой "Привет!" и переводит его в состояние ECHO.
     Теперь в ответ на его команды будет запускаеться хэндлер echo.
     """
-    update.message.reply_text(text='Привет!')
+    keyboard = [
+        [
+            InlineKeyboardButton("Option 1", callback_data='1'),
+            InlineKeyboardButton("Option 2", callback_data='2'),
+        ],
+        [InlineKeyboardButton("Option 3", callback_data='3')],
+    ]
+
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    update.message.reply_text(text='Привет!', reply_markup=reply_markup)
     return "ECHO"
 
 
