d3c44f6d1039dc0ce476f7df6ff7f37436b8ec33
VneTraffiqua <safe72again@gmail.com>
2023-10-06 04:04:48 -0700

    Update strapi_commands.py

--- strapi_commands.py
+++ strapi_commands.py
@@ -7,31 +7,39 @@ strapi_token = env.str('STRAPI_TOKEN')
 strapi_url = env.str('STRAPI_URL')
 
 
-def get_cart_items(update, context):
-    query = update.callback_query
-    query.answer()
-    context.bot.delete_message(chat_id=query.message.chat_id,
-                               message_id=query.message.message_id,
-                               )
-    url = f'{strapi_url}/api/carts/{query.message.chat_id}'
+def get_cart_items(chat_id):
+    url = f'{strapi_url}/api/carts/{chat_id}'
     header = {
         'Authorization': f'bearer {strapi_token}'
     }
     params = {
         'populate': ['cart_products', 'cart_products.product']
     }
+    response = requests.get(url, headers=header, params=params)
+    response.raise_for_status()
+    return response.json()
+
+
+def get_or_create_cart(strapi_token, tg_user_id):
     try:
-        response = requests.get(url, headers=header, params=params)
-        response.raise_for_status()
-        cart_items = response.json()
+        cart_items = get_cart_items(tg_user_id)
+        return cart_items
     except requests.exceptions.HTTPError:
-        create_cart(strapi_token, query.message.chat_id)
-    finally:
+        url = f'http://localhost:1337/api/carts'
+        header = {
+            'Authorization': f'bearer {strapi_token}'
+        }
+        data = {
+            'data': {"id": tg_user_id}
+        }
+        response = requests.post(url, headers=header, json=data)
+        response.raise_for_status()
+        cart_items = get_cart_items(tg_user_id)
         return cart_items
 
 
 def get_shop_items(strapi_token):
-    url = f'{strapi_url}/api/products'
+    url = f'{strapi_url}/api/products/'
     header = {
         'Authorization': f'bearer {strapi_token}'
     }
@@ -53,38 +61,8 @@ def get_item_by_id(strapi_token, item_id):
     return response.json()
 
 
-def create_cart(strapi_token, tg_user_id):
-    url = f'{strapi_url}/api/carts'
-    header = {
-        'Authorization': f'bearer {strapi_token}'
-    }
-    data = {
-        'data': {"id": tg_user_id}
-    }
-    response = requests.post(url, headers=header)
-    response.raise_for_status()
-    return
-
-
-def checkout(chat_id, user_reply):
-    try:
-        url = f'{strapi_url}/api/tg-users/'
-        header = {
-            'Authorization': f'bearer {strapi_token}'
-        }
-        data = {
-            'data': {"id": chat_id, "email": user_reply, "cart": chat_id}
-        }
-        response = requests.post(url, headers=header, json=data)
-        response.raise_for_status()
-        delete_cart_products(strapi_token, chat_id)
-        return
-    except requests.exceptions.HTTPError:
-        return
-
-
 def add_item_in_cart(strapi_token, tg_user_id, item_id):
-    url = f'{strapi_url}/api/cart-products'
+    url = f'{strapi_url}/api/cart-products/'
     header = {
         'Authorization': f'bearer {strapi_token}'
     }
@@ -99,7 +77,10 @@ def add_item_in_cart(strapi_token, tg_user_id, item_id):
         response.raise_for_status()
         return
     except requests.exceptions.HTTPError:
-        create_cart(strapi_token, tg_user_id)
+        get_or_create_cart(strapi_token, tg_user_id)
+        response = requests.post(url, headers=header, json=data)
+        response.raise_for_status()
+        return
 
 
 def delete_cart_products(strapi_token, tg_user_id):
